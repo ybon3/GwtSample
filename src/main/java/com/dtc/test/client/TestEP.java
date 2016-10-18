060ca1e8857686b11a9a6081c1628cf37003ede5
ybon3 <lee.ybon@gmail.com>
2016-10-18 09:31:45 +0800

    valueProvider test 版

--- src/main/java/com/dtc/test/client/TestEP.java
+++ src/main/java/com/dtc/test/client/TestEP.java
@@ -1,73 +1,140 @@
 package com.dtc.test.client;
 
-import java.util.Date;
+import java.util.ArrayList;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.Scheduler;
-import com.google.gwt.core.client.Scheduler.RepeatingCommand;
-import com.google.gwt.core.shared.GWT;
-import com.google.gwt.i18n.client.DateTimeFormat;
-import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.sencha.gxt.core.client.ValueProvider;
+import com.sencha.gxt.core.client.util.Margins;
+import com.sencha.gxt.data.shared.ListStore;
+import com.sencha.gxt.data.shared.ModelKeyProvider;
 import com.sencha.gxt.widget.core.client.button.TextButton;
+import com.sencha.gxt.widget.core.client.container.HorizontalLayoutContainer;
+import com.sencha.gxt.widget.core.client.container.HorizontalLayoutContainer.HorizontalLayoutData;
 import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
+import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer.VerticalLayoutData;
+import com.sencha.gxt.widget.core.client.container.Viewport;
 import com.sencha.gxt.widget.core.client.event.SelectEvent;
 import com.sencha.gxt.widget.core.client.event.SelectEvent.SelectHandler;
-import com.sencha.gxt.widget.core.client.form.DateField;
+import com.sencha.gxt.widget.core.client.grid.ColumnConfig;
+import com.sencha.gxt.widget.core.client.grid.ColumnModel;
+import com.sencha.gxt.widget.core.client.grid.Grid;
+
+import us.dontcareabout.gxt.client.model.GetValueProvider;
 
 public class TestEP implements EntryPoint {
-	private static final RpcServiceAsync rpc = GWT.create(RpcService.class);
+	// 用來表現一般情況的
+	private static final ValueProvider<FooBar, String> normalVP = new GetValueProvider<FooBar, String>() {
+		@Override
+		public String getValue(FooBar object) {
+			if(object.getFoo() == null) return "";
+			if(object.getFoo().getBar() == null) return "";
+			return object.getFoo().getBar().getValue();
+		}
+	};
 
-	private DateField dateField = new DateField();
-	private TextButton button = new TextButton("送出");
-	private boolean rpcFinish = false;
-	private int counter = 0;
-	private DateTimeFormat format = DateTimeFormat.getFormat("HH:mm:ss");
+	// 用來測試 try-catch style
+	private static final ValueProvider<FooBar, String> trycatVP = new GetValueProvider<FooBar, String>() {
+		@Override
+		public String getValue(FooBar object) {
+			try {return object.getFoo().getBar().getValue();}
+			catch (Exception e) {return "";}
+		}
+	};
+
+	private static MyGrid normalGrid = new MyGrid(genColumnModel(normalVP));
+	private static MyGrid tryCatGrid = new MyGrid(genColumnModel(trycatVP));
+	private static TextButton btnNormal = new TextButton("Normal");
+	private static TextButton btnTryCat = new TextButton("TryCatch");
+
+	static {
+		btnNormal.addSelectHandler(new SelectHandler(){
+			@Override
+			public void onSelect(SelectEvent event) {
+				long cnt = System.currentTimeMillis();
+				for (int i = 0; i < 500; i++) {
+					normalGrid.getStore().clear();
+					normalGrid.getStore().addAll(TestData.DATA);
+				}
+				GWT.log("[NORMAL] used time millis: " + (System.currentTimeMillis() - cnt));
+			}
+		});
+
+		btnTryCat.addSelectHandler(new SelectHandler(){
+			@Override
+			public void onSelect(SelectEvent event) {
+				long cnt = System.currentTimeMillis();
+				for (int i = 0; i < 500; i++) {
+					tryCatGrid.getStore().clear();
+					tryCatGrid.getStore().addAll(TestData.DATA);
+				}
+				GWT.log("[TRY CATCH] used time millis: " + (System.currentTimeMillis() - cnt));
+			}
+		});
+	}
 
 	@Override
 	public void onModuleLoad() {
-		button.addSelectHandler(new SelectHandler() {
-			@Override
-			public void onSelect(SelectEvent event) {
-				start();
-			}
-		});
-		
-		VerticalLayoutContainer vc = new VerticalLayoutContainer();
-		vc.add(dateField);
-		vc.add(button);
-
-		RootPanel.get().add(vc);
+		Viewport vp = new Viewport();
+		HorizontalLayoutContainer hlc = new HorizontalLayoutContainer();
+		HorizontalLayoutData hld = new HorizontalLayoutData(0.5, 1);
+		VerticalLayoutContainer leftVLC = new VerticalLayoutContainer();
+		VerticalLayoutContainer rightVLC = new VerticalLayoutContainer();
+		VerticalLayoutData buttonVLD = new VerticalLayoutData(1, 50, new Margins(10));
+		VerticalLayoutData GridVLD = new VerticalLayoutData(1, 1, new Margins(10));
+		leftVLC.add(btnNormal, buttonVLD);
+		leftVLC.add(normalGrid, GridVLD);
+		rightVLC.add(btnTryCat, buttonVLD);
+		rightVLC.add(tryCatGrid, GridVLD);
+		hlc.add(leftVLC, hld);
+		hlc.add(rightVLC, hld);
+		vp.add(hlc);
+		RootPanel.get().add(vp);
 	}
-	
-	private void start() {
-		GWT.log("開始啦開始啦～～～～");
-		counter = 0;
-		rpcFinish = false;
-		
-		//發 RPC
-		rpc.before(dateField.getValue(), new AsyncCallback<Boolean>() {
+
+	private static ColumnModel<FooBar> genColumnModel(ValueProvider<FooBar, String> vp) {
+		ArrayList<ColumnConfig<FooBar, ?>> list = new ArrayList<>();
+		list.add(new ColumnConfig<FooBar, String>(vp, 20, "DATA LIST"));
+		list.add(new ColumnConfig<FooBar, String>(vp, 20, "DATA LIST"));
+		list.add(new ColumnConfig<FooBar, String>(vp, 20, "DATA LIST"));
+		list.add(new ColumnConfig<FooBar, String>(vp, 20, "DATA LIST"));
+		list.add(new ColumnConfig<FooBar, String>(vp, 20, "DATA LIST"));
+		return new ColumnModel<>(list);
+	}
+}
+
+class MyGrid extends Grid<FooBar> {
+	public MyGrid(ColumnModel<FooBar> cm) {
+		super(new ListStore<FooBar>(new ModelKeyProvider<FooBar>() {
 			@Override
-			public void onSuccess(Boolean result) {
-				GWT.log(result + "你的時間比較" + (result ? "早" : "晚"));
-				rpcFinish = true;
+			public String getKey(FooBar item) {
+				return item.getId();
 			}
-			
-			@Override
-			public void onFailure(Throwable caught) {
-				GWT.log("出問題啦！訊息：" + caught.getMessage());
-				rpcFinish = true;
-			}
-		});
-		
-		//每隔 1 秒鐘寫一行 log 以顯示出 async 的行為
-		Scheduler.get().scheduleFixedPeriod(new RepeatingCommand() {
-			@Override
-			public boolean execute() {
-				counter++;
-				GWT.log("第 " + counter + " 次 @ " + format.format(new Date()));
-				return !rpcFinish;
-			}
-		}, 1000);		
+		}), cm);
+		getView().setForceFit(true);
+	}
+}
+
+class FooBar {
+	private String id;
+	private Foo foo;
+	public FooBar(String id, String value) {
+		this.id = id;
+		this.foo = new Foo(value);
+	}
+	public Foo getFoo() { return foo; }
+	public String getId() { return id; }
+
+	class Foo {
+		private Bar bar;
+		public Foo(String value) { this.bar = new Bar(value); }
+		public Bar getBar() { return bar; }
+
+		class Bar {
+			private String value;
+			public Bar(String value) { this.value = value; }
+			public String getValue() { return value; }
+		}
 	}
 }
