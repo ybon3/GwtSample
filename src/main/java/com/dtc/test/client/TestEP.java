f283ac0e9ab53f1f8bad854b5401e26adcedfdba
ybon3 <lee.ybon@gmail.com>
2016-10-20 10:52:21 +0800

    commit

--- src/main/java/com/dtc/test/client/TestEP.java
+++ src/main/java/com/dtc/test/client/TestEP.java
@@ -1,50 +1,29 @@
 package com.dtc.test.client;
 
-import java.util.ArrayList;
-
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.sencha.gxt.core.client.ValueProvider;
-import com.sencha.gxt.core.client.util.Margins;
-import com.sencha.gxt.data.shared.ListStore;
-import com.sencha.gxt.data.shared.ModelKeyProvider;
 import com.sencha.gxt.widget.core.client.button.TextButton;
 import com.sencha.gxt.widget.core.client.container.HorizontalLayoutContainer;
 import com.sencha.gxt.widget.core.client.container.HorizontalLayoutContainer.HorizontalLayoutData;
-import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
-import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer.VerticalLayoutData;
 import com.sencha.gxt.widget.core.client.container.Viewport;
 import com.sencha.gxt.widget.core.client.event.SelectEvent;
 import com.sencha.gxt.widget.core.client.event.SelectEvent.SelectHandler;
-import com.sencha.gxt.widget.core.client.grid.ColumnConfig;
-import com.sencha.gxt.widget.core.client.grid.ColumnModel;
-import com.sencha.gxt.widget.core.client.grid.Grid;
-
-import us.dontcareabout.gxt.client.model.GetValueProvider;
 
 public class TestEP implements EntryPoint {
 	// 用來表現一般情況的
-	private static final ValueProvider<FooBar, String> normalVP = new GetValueProvider<FooBar, String>() {
-		@Override
-		public String getValue(FooBar object) {
-			if(object.getFoo() == null) return "";
-			if(object.getFoo().getBar() == null) return "";
-			return object.getFoo().getBar().getValue();
-		}
-	};
+	public static String getValueNormal(FooBar object) {
+		if(object.getFoo() == null) return "";
+		if(object.getFoo().getBar() == null) return "boom!!";
+		return object.getFoo().getBar().getValue();
+	}
 
 	// 用來測試 try-catch style
-	private static final ValueProvider<FooBar, String> trycatVP = new GetValueProvider<FooBar, String>() {
-		@Override
-		public String getValue(FooBar object) {
-			try {return object.getFoo().getBar().getValue();}
-			catch (Exception e) {return "";}
-		}
-	};
+	public static String getValueTryCatch(FooBar object) {
+		try {return object.getFoo().getBar().getValue();}
+		catch (Exception e) {return "boom!";}
+	}
 
-	private static MyGrid normalGrid = new MyGrid(genColumnModel(normalVP));
-	private static MyGrid tryCatGrid = new MyGrid(genColumnModel(trycatVP));
 	private static TextButton btnNormal = new TextButton("Normal");
 	private static TextButton btnTryCat = new TextButton("TryCatch");
 
@@ -53,9 +32,14 @@ public class TestEP implements EntryPoint {
 			@Override
 			public void onSelect(SelectEvent event) {
 				long cnt = System.currentTimeMillis();
-				for (int i = 0; i < 500; i++) {
-					normalGrid.getStore().clear();
-					normalGrid.getStore().addAll(TestData.DATA);
+				for (int i = 0; i < 50; i++) {
+					for (FooBar data : TestData.DATA) {
+						getValueNormal(data);
+						getValueNormal(data);
+						getValueNormal(data);
+						getValueNormal(data);
+						getValueNormal(data);
+					}
 				}
 				GWT.log("[NORMAL] used time millis: " + (System.currentTimeMillis() - cnt));
 			}
@@ -65,9 +49,14 @@ public class TestEP implements EntryPoint {
 			@Override
 			public void onSelect(SelectEvent event) {
 				long cnt = System.currentTimeMillis();
-				for (int i = 0; i < 500; i++) {
-					tryCatGrid.getStore().clear();
-					tryCatGrid.getStore().addAll(TestData.DATA);
+				for (int i = 0; i < 50; i++) {
+					for (FooBar data : TestData.DATA) {
+						getValueTryCatch(data);
+						getValueTryCatch(data);
+						getValueTryCatch(data);
+						getValueTryCatch(data);
+						getValueTryCatch(data);
+					}
 				}
 				GWT.log("[TRY CATCH] used time millis: " + (System.currentTimeMillis() - cnt));
 			}
@@ -79,40 +68,10 @@ public class TestEP implements EntryPoint {
 		Viewport vp = new Viewport();
 		HorizontalLayoutContainer hlc = new HorizontalLayoutContainer();
 		HorizontalLayoutData hld = new HorizontalLayoutData(0.5, 1);
-		VerticalLayoutContainer leftVLC = new VerticalLayoutContainer();
-		VerticalLayoutContainer rightVLC = new VerticalLayoutContainer();
-		VerticalLayoutData buttonVLD = new VerticalLayoutData(1, 50, new Margins(10));
-		VerticalLayoutData GridVLD = new VerticalLayoutData(1, 1, new Margins(10));
-		leftVLC.add(btnNormal, buttonVLD);
-		leftVLC.add(normalGrid, GridVLD);
-		rightVLC.add(btnTryCat, buttonVLD);
-		rightVLC.add(tryCatGrid, GridVLD);
-		hlc.add(leftVLC, hld);
-		hlc.add(rightVLC, hld);
+		hlc.add(btnNormal, hld);
+		hlc.add(btnTryCat, hld);
 		vp.add(hlc);
 		RootPanel.get().add(vp);
-	}
-
-	private static ColumnModel<FooBar> genColumnModel(ValueProvider<FooBar, String> vp) {
-		ArrayList<ColumnConfig<FooBar, ?>> list = new ArrayList<>();
-		list.add(new ColumnConfig<FooBar, String>(vp, 20, "DATA LIST"));
-		list.add(new ColumnConfig<FooBar, String>(vp, 20, "DATA LIST"));
-		list.add(new ColumnConfig<FooBar, String>(vp, 20, "DATA LIST"));
-		list.add(new ColumnConfig<FooBar, String>(vp, 20, "DATA LIST"));
-		list.add(new ColumnConfig<FooBar, String>(vp, 20, "DATA LIST"));
-		return new ColumnModel<>(list);
-	}
-}
-
-class MyGrid extends Grid<FooBar> {
-	public MyGrid(ColumnModel<FooBar> cm) {
-		super(new ListStore<FooBar>(new ModelKeyProvider<FooBar>() {
-			@Override
-			public String getKey(FooBar item) {
-				return item.getId();
-			}
-		}), cm);
-		getView().setForceFit(true);
 	}
 }
 
@@ -123,13 +82,13 @@ class FooBar {
 		this.id = id;
 		this.foo = new Foo(value);
 	}
-	public Foo getFoo() { return foo; }
+	public Foo getFoo() { return null; } //foo; }
 	public String getId() { return id; }
 
 	class Foo {
 		private Bar bar;
 		public Foo(String value) { this.bar = new Bar(value); }
-		public Bar getBar() { return bar; }
+		public Bar getBar() { return null; }// bar; }
 
 		class Bar {
 			private String value;
