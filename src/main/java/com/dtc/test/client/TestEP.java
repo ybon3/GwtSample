73dfa998aa21642aceae7bd4cb4dcc0413c9fe05
ybon3 <lee.ybon@gmail.com>
2016-10-21 08:43:22 +0800

    測試結果可參考 http://gwt.dontcareabout.us/GXT/Component.html 裡面的 comboBox 段落

--- src/main/java/com/dtc/test/client/TestEP.java
+++ src/main/java/com/dtc/test/client/TestEP.java
@@ -1,73 +1,84 @@
 package com.dtc.test.client;
 
-import java.util.Date;
+import java.util.ArrayList;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.Scheduler;
-import com.google.gwt.core.client.Scheduler.RepeatingCommand;
 import com.google.gwt.core.shared.GWT;
-import com.google.gwt.i18n.client.DateTimeFormat;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.sencha.gxt.data.shared.LabelProvider;
+import com.sencha.gxt.data.shared.ListStore;
+import com.sencha.gxt.data.shared.ModelKeyProvider;
 import com.sencha.gxt.widget.core.client.button.TextButton;
 import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
 import com.sencha.gxt.widget.core.client.event.SelectEvent;
 import com.sencha.gxt.widget.core.client.event.SelectEvent.SelectHandler;
-import com.sencha.gxt.widget.core.client.form.DateField;
+import com.sencha.gxt.widget.core.client.form.ComboBox;
 
 public class TestEP implements EntryPoint {
-	private static final RpcServiceAsync rpc = GWT.create(RpcService.class);
-
-	private DateField dateField = new DateField();
+	private ComboBox<MyData> cb;
 	private TextButton button = new TextButton("送出");
-	private boolean rpcFinish = false;
-	private int counter = 0;
-	private DateTimeFormat format = DateTimeFormat.getFormat("HH:mm:ss");
 
 	@Override
 	public void onModuleLoad() {
+		cb = new ComboBox<MyData>(
+				new ListStore<MyData>(new ModelKeyProvider<MyData>(){
+					@Override
+					public String getKey(MyData item) {
+						return item.getKey();
+					}
+				}),
+				new LabelProvider<MyData>(){
+					@Override
+					public String getLabel(MyData item) {
+						return item.getValue();
+					}
+				}
+			);
+
 		button.addSelectHandler(new SelectHandler() {
 			@Override
 			public void onSelect(SelectEvent event) {
-				start();
+				GWT.log(cb.getValue().getKey());
 			}
 		});
-		
-		VerticalLayoutContainer vc = new VerticalLayoutContainer();
-		vc.add(dateField);
-		vc.add(button);
 
+		//你們念 Nikon 可是我念 Nikon，但是原來應該要念 Nikon
+
+		ArrayList<MyData> dataList = new ArrayList<>();
+		dataList.add(new MyData("123", "好棒棒"));
+		dataList.add(new MyData("456", "好棒棒"));
+		dataList.add(new MyData("789", "好棒棒"));
+		dataList.add(new MyData("123aaa", "a好棒棒"));
+		dataList.add(new MyData("456bbb", "b好棒棒"));
+		dataList.add(new MyData("789ccc", "c好棒棒"));
+		cb.getStore().addAll(dataList);
+
+		VerticalLayoutContainer vc = new VerticalLayoutContainer();
+		vc.add(cb);
+		vc.add(button);
 		RootPanel.get().add(vc);
 	}
-	
-	private void start() {
-		GWT.log("開始啦開始啦～～～～");
-		counter = 0;
-		rpcFinish = false;
-		
-		//發 RPC
-		rpc.before(dateField.getValue(), new AsyncCallback<Boolean>() {
-			@Override
-			public void onSuccess(Boolean result) {
-				GWT.log(result + "你的時間比較" + (result ? "早" : "晚"));
-				rpcFinish = true;
-			}
-			
-			@Override
-			public void onFailure(Throwable caught) {
-				GWT.log("出問題啦！訊息：" + caught.getMessage());
-				rpcFinish = true;
-			}
-		});
-		
-		//每隔 1 秒鐘寫一行 log 以顯示出 async 的行為
-		Scheduler.get().scheduleFixedPeriod(new RepeatingCommand() {
-			@Override
-			public boolean execute() {
-				counter++;
-				GWT.log("第 " + counter + " 次 @ " + format.format(new Date()));
-				return !rpcFinish;
-			}
-		}, 1000);		
+
+	class MyData {
+		private String key;
+		private String value;
+
+		public MyData(String key, String value) {
+			super();
+			this.key = key;
+			this.value = value;
+		}
+		public String getKey() {
+			return key;
+		}
+		public void setKey(String key) {
+			this.key = key;
+		}
+		public String getValue() {
+			return value;
+		}
+		public void setValue(String value) {
+			this.value = value;
+		}
 	}
 }
